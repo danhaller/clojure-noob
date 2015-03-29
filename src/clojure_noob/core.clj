423c6fda6e9cd08d78068a520d08ac653f15348e
Dan Haller <dskhaller@gmail.com>
2015-03-29 14:56:19 +0100

    Programming to abstractions ongoing

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -246,3 +246,111 @@
       (recur rest (+ accumulated-size (:size part)))))))
 
 (hit asym-hobbit-body-parts)
+
+; Core functions
+
+(defn titleise
+  [topic]
+  (str topic " for the brave and true"))
+
+(titleise "Clojure")
+(map titleise ["Hamsters" "Ragnarok"])
+(map titleise '("Hamsters" "Ragnarok"))
+(map titleise #{"Elbows" "Soap Carving"})
+
+
+(defn label-key-val
+  [[key val]]
+  (str "key: " key ", val: " val))
+
+(map label-key-val { :name "Edward" :occupation "Perennial high-schooler" })
+
+(into {}
+(map (fn [[key val]] [key (inc val)])
+  {:min 30 :max 40}))
+
+(identity "Stephen Salvatore")
+
+(def bill {:name "Bill" :occupation "Dead mopey guy"})
+(map identity bill)
+(seq bill)
+
+(map str ["a" "b" "c"] ["A" "B" "C"])
+
+(def human-consumption   [8.1 7.3 6.6 5.0])
+(def critter-consumption [0.0 0.2 0.3 1.1])
+
+(defn unify-diet-data
+  [human critter]
+  {:human human :critter critter})
+
+(map unify-diet-data human-consumption critter-consumption)
+
+(def sum #(reduce + %))
+(sum [1 2 3])
+(def avg #(/ (sum %) (count %)))
+(avg [1 2 3])
+(defn stats
+  [numbers]
+  (map #(% numbers) [sum count avg]))
+
+(stats [3 4 10])
+
+(take 3 [1 2 3 4 5 6 7 8 9 10])
+
+(drop 3 [1 2 3 4 5 6 7 8 9 10])
+
+(def food-journal
+  [{:month 1 :day 1 :human 5.3 :critter 2.3}
+   {:month 1 :day 2 :human 5.1 :critter 2.0}
+   {:month 2 :day 1 :human 4.9 :critter 2.1}
+   {:month 2 :day 2 :human 5.0 :critter 2.5}
+   {:month 3 :day 1 :human 4.2 :critter 3.3}
+   {:month 3 :day 2 :human 4.0 :critter 3.8}
+   {:month 4 :day 1 :human 3.7 :critter 3.9}
+   {:month 4 :day 2 :human 3.7 :critter 3.6}])
+
+(take-while #(< (:month %) 3) food-journal)
+(drop-while #(< (:month %) 3) food-journal)
+
+(take-while #(< (:month %) 4)
+            (drop-while #(< (:month %) 2) food-journal))
+
+(filter #(< (:human %) 5.0) food-journal)
+(filter #(< (:month %) 3.0) food-journal)
+
+(some #(> (:critter %) 5) food-journal)
+(some #(> (:critter %) 3) food-journal)
+(some #(and (> (:critter %) 3) %) food-journal)
+
+(sort [3 2 1])
+(sort-by count ["aaa" "b" "cc"])
+
+(concat [1 2] [3 4])
+
+(def vampire-database
+  {0 {:makes-blood-puns? false, :has-pulse? true  :name "McFishwich"}
+   1 {:makes-blood-puns? false, :has-pulse? true  :name "McMackson"}
+   2 {:makes-blood-puns? true,  :has-pulse? false :name "Damon Salvatore"}
+   3 {:makes-blood-puns? true,  :has-pulse? true  :name "Mickey Mouse"}})
+
+(defn vampire-related-details
+  [social-security-number]
+  (Thread/sleep 1000)
+  (get vampire-database social-security-number))
+
+(defn vampire?
+  [record]
+  (and (:makes-blood-puns? record)
+       (not (:has-pulse? record))))
+
+(defn identify-vampire
+  [social-security-numbers]
+  (first (filter vampire?
+                 (map vampire-related-details social-security-numbers))))
+
+(time (identify-vampire (range 0 1000000)))
+
+(concat (repeat 8 "na") ["Batman"])
+
+(take 3 (repeatedly (fn [] (rand-int 10))))
